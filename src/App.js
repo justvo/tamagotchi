9e00cc05c6a1fabd9ff2b43f152fea0e337e9c32
Vlad <129364256+justvo@users.noreply.github.com>
2023-10-04 20:02:01 +0000

    test-app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-
+  const [name, setName] = useState("");
+  
   return (
-    
+    <div className='App'>
+      <label className='lable'>Name</label>
+      <input
+      type = "text"
+      value={name}
+      onChange={(e)=>setName(e.target.value)}>
+      </input>
+      <p>Hi, {name}</p>
+    </div>
   );
+
 }
 
 export default App;
